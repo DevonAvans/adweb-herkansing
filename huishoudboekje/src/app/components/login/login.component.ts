e65f45eaa67730cf9207fb6f3f5200f0b371fbdc
Devon Thomassen <devon.thomassen@hotmail.com>
2024-06-01 22:24:19 +0200

    het scheiden van verantwoordelijkheid tussen auth en user services

--- huishoudboekje/src/app/components/login/login.component.ts
+++ huishoudboekje/src/app/components/login/login.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "@services/auth.service";
 
 @Component({
-	selector: "app-login",
-	standalone: true,
-	imports: [],
-	templateUrl: "./login.component.html",
-	styleUrl: "./login.component.scss",
+    selector: "app-login",
+    standalone: true,
+    imports: [],
+    templateUrl: "./login.component.html",
+    styleUrl: "./login.component.scss",
 })
 export class LoginComponent implements OnInit {
-	public user: any;
+    constructor(private authService: AuthService) {}
 
-	constructor(private authService: AuthService) {}
+    ngOnInit(): void {}
 
-	ngOnInit(): void {}
-
-	loginWithGoogle() {
-		this.authService.login();
-	}
+    loginWithGoogle() {
+        this.authService.loginWithGoogle();
+    }
 }
